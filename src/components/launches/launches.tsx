578ae52b9410119ff653c0a5856a90e84b4e2ac3
krisgoorhuis <krisgoorhuis@gmail.com>
2021-11-12 07:58:45 -0500

    More experimenting with Netlify

--- src/components/launches/launches.tsx
+++ src/components/launches/launches.tsx
@@ -27,19 +27,27 @@ export default function Launches() {
 
   const MyComponent1editedagain = (props: { data: Launch[] }) => {
     const array = [1, 2, 3]
-    console.log("props.data from editedagain")
-    console.log(props.data)
     return (
       <SimpleGrid m={[2, null, 6]} minChildWidth="350px" spacing="4">
         {
           (props.data.flat() || array).map((item, i) => {
-            console.log("item")
-            console.log(item)
             return (
-              <div>things2again
+              <div>
                 <LaunchItem launch={item} key={item.flight_number + i} />
-
               </div>
+            )
+          })
+        }
+      </SimpleGrid>
+    )
+  }
+  const MyComponent1editedagainagain = (props: { data: Launch[] }) => {
+    return (
+      <SimpleGrid m={[2, null, 6]} minChildWidth="350px" spacing="4">
+        {
+          (props.data.flat()).map((item, i) => {
+            return (
+              <LaunchItem launch={item} key={item.flight_number + i} />
             )
           })
         }
@@ -53,9 +61,9 @@ export default function Launches() {
       <Breadcrumbs
         items={[{ label: "Home", to: "/" }, { label: "Launches" }]}
       />
-      {/* <SimpleGrid m={[2, null, 6]} minChildWidth="350px" spacing="4"> */}
 
       <MyComponent1editedagain data={data || [ExampleLaunch]} />
+      <MyComponent1editedagainagain data={data || [ExampleLaunch]} />
 
       <SimpleGrid >
         {error && <Error />}
