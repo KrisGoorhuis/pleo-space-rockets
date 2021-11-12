d3208e6440f31d93107fb5965317a7b64dc634ed
krisgoorhuis <krisgoorhuis@gmail.com>
2021-11-12 07:55:08 -0500

    More testing Netlify

--- src/components/launches/launches.tsx
+++ src/components/launches/launches.tsx
@@ -1,21 +1,19 @@
 import React from "react";
-// import { SimpleGrid } from "@chakra-ui/react";
+import { SimpleGrid } from "@chakra-ui/react";
 
 import { useSpaceXPaginated } from "../../utils/use-space-x";
-// import Error from "../error";
+import Error from "../error";
 import Breadcrumbs from "../breadcrumbs";
 import LoadMoreButton from "../load-more-button";
-// import { LaunchItem } from "./launchItem";
+import { LaunchItem } from "./launchItem";
 import { Launch } from "../../model";
 import { ExampleLaunch } from "../../model/example-launch";
-import { SimpleGrid } from "@chakra-ui/layout";
 
 
 const PAGE_SIZE = 12;
 
 export default function Launches() {
-  // const { data, error, isValidating, setSize, size } = useSpaceXPaginated<Launch>(
-  const { data, isValidating, setSize, size } = useSpaceXPaginated<Launch>(
+  const { data, error, isValidating, setSize, size } = useSpaceXPaginated<Launch>(
     "/launches/past",
     {
       limit: PAGE_SIZE,
@@ -27,70 +25,20 @@ export default function Launches() {
   console.log("data")
   console.log(data)
 
-
-  const MyComponent = () => {
-    const array = [1, 2, 3]
-    console.log()
-    return (
-      <SimpleGrid m={[2, null, 6]} minChildWidth="350px" spacing="4">
-        {
-          array.map(() => {
-            return (
-              <div>things</div>
-            )
-          })
-        }
-      </SimpleGrid>
-    )
-  }
-
-  // const MyComponent1edited = () => {
-  //   const array = [1, 2, 3]
-  //   console.log()
-  //   return (
-  //     <SimpleGrid m={[2, null, 6]} minChildWidth="350px" spacing="4">
-  //       {
-  //         (data || array).map(() => {
-  //           return (
-  //             <div>things2</div>
-  //           )
-  //         })
-  //       }
-  //     </SimpleGrid>
-  //   )
-  // }
-  const MyComponent1editedagain = (props: {data: Launch[]}) => {
+  const MyComponent1editedagain = (props: { data: Launch[] }) => {
     const array = [1, 2, 3]
     console.log("props.data from editedagain")
     console.log(props.data)
     return (
       <SimpleGrid m={[2, null, 6]} minChildWidth="350px" spacing="4">
         {
-          (props.data.flat() || array).map((item) => {
+          (props.data.flat() || array).map((item, i) => {
             console.log("item")
             console.log(item)
             return (
-              <div>things2again</div>
-            )
-          })
-        }
-      </SimpleGrid>
-    )
-  }
-  const MyComponent2 = (props: {data: Launch[]}) => {
-    const array = [1, 2, 3]
-    console.log("props.data from editedagain")
-    console.log(props.data)
-    return (
-      <SimpleGrid m={[2, null, 6]} minChildWidth="350px" spacing="4">
-        {
-          (props.data.flat() || array).map((item) => {
-            console.log("item")
-            console.log(item)
-            return (
-              <div>2
-                
-                <div>{item.mission_id}</div>
+              <div>things2again
+                <LaunchItem launch={item} key={item.flight_number + i} />
+
               </div>
             )
           })
@@ -98,23 +46,7 @@ export default function Launches() {
       </SimpleGrid>
     )
   }
-  // const MyComponent3 = () => {
-  //   return (
-  //     <div>
-  //       {([1,2,3]).map(() => {
-  //         console.log("This ran")
-  //         return ( // Key change used for testing purposes
-  //           <div>
-  //             there is a launch item in the same div as me
-  //             <div>text</div>
-  //             {/* <LaunchItem launch={launch} key={launch.flight_number + i} /> */}
-  //           </div>
-  //         )
-  //       })
-  //       }
-  //     </div>
-  //   )
-  // }
+
 
   return (
     <div>
@@ -122,24 +54,10 @@ export default function Launches() {
         items={[{ label: "Home", to: "/" }, { label: "Launches" }]}
       />
       {/* <SimpleGrid m={[2, null, 6]} minChildWidth="350px" spacing="4"> */}
-      <div>
-        <MyComponent />
-      </div>
-      component 2 outer
-      <div>
-        <MyComponent2 data={data || []} />
-      </div>
-      component3
-      {/* <div>
-        <MyComponent3 />
-      </div> */}
 
-      <div>
-        <MyComponent1editedagain data={data || [ExampleLaunch]} />
-      </div>
       <MyComponent1editedagain data={data || [ExampleLaunch]} />
 
-      {/* <div >
+      <SimpleGrid >
         {error && <Error />}
         {data &&
           data
@@ -147,7 +65,7 @@ export default function Launches() {
             .map((launch, i) => ( // Key change used for testing purposes
               <LaunchItem launch={launch} key={launch.flight_number + i} />
             ))}
-      </div> */}
+      </SimpleGrid>
       <LoadMoreButton
         loadMore={() => size && setSize && setSize(size + 1)}
         data={data}
